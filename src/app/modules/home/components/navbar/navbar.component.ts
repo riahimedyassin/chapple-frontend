5eb0b3134affddd7eff647799c0295c6be864af1
riahimedyassin <riahimohamedyassin@charguia.r-iset.tn>
2024-07-24 11:08:28 +0100

    Updated landing page UI

--- src/app/modules/home/components/navbar/navbar.component.ts
+++ src/app/modules/home/components/navbar/navbar.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
 
+interface Page {
+  name: string;
+  route: string;
+}
+
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent {
-
+  pages: Page[] = [
+    {
+      name: 'Home',
+      route: 'home',
+    },
+    {
+      name: 'About',
+      route: 'about',
+    },
+    {
+      name: 'Contribute',
+      route: 'contribute',
+    },
+  ];
 }
